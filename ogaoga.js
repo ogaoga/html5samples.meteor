05e03a39343379ef3fc8c43ce29c175772c449d8
Tsutomu Ogasawara <info@ogaoga.org>
2012-06-21 22:36:39 +0900

    Added backbone.js and routing.

--- ogaoga.js
+++ ogaoga.js
@@ -1,8 +1,31 @@
 if (Meteor.is_client) {
+  //
+  // Routing
+  //
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      "*actions": "defaultRoute" // matches http://example.com/#anything-here
+    },
+    defaultRoute: function( actions ){
+      // The variable passed in matches the variable in the route definition "actions"
+      if ( actions == 'css' ) {
+        $('article').html(Template.section_css());
+      }
+      else {
+        $('article').html(Template.home());
+      }
+    },
+  });
+  // Initiate the router
+  var app_router = new AppRouter;
+  // Start Backbone history a neccesary step for bookmarkable URL's
+  Backbone.history.start();
+
+  /*
+  // template
   Template.hello.greeting = function () {
     return "Welcome to ogaoga.";
   };
-
   Template.hello.events = {
     'click input' : function () {
       // template data, if any, is available in 'this'
@@ -10,10 +33,13 @@ if (Meteor.is_client) {
         console.log("You pressed the button");
     }
   };
+  */
 }
 
+/*
 if (Meteor.is_server) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
 }
+*/
