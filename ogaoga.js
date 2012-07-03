d6f979bcf2fc647d1851f01a687ca36d0accb643
Tsutomu Ogasawara <info@ogaoga.org>
2012-07-03 22:16:25 +0900

    ...

--- ogaoga.js
+++ ogaoga.js
@@ -21,6 +21,23 @@ if (Meteor.is_client) {
   // Start Backbone history a neccesary step for bookmarkable URL's
   Backbone.history.start();
 
+  // 
+  // breadcrumbs
+  //
+  Template.breadcrumbs.pages = function() {
+    return [
+      {path: "/", label: "Home"},
+      {path: null, label: "CSS Styling"},
+    ];
+  };
+  Meteor.ui.render(function() {
+    var html = Template.breadcrumbs({pages: [
+      {path: "/", label: "Home"},
+      {path: null, label: "CSS Styling"},
+    ]});
+    console.log(html);
+  })
+
   /*
   // template
   Template.hello.greeting = function () {
